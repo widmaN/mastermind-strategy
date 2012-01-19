e21efd71866d74e823cdc030e025a3438b9e7052
fancitron <fancitron@gmail.com>
2012-01-19 01:11:59 +0000

    Re-implemented HRTimer using C++11 <chrono> library.

--- src/HRTimer.h
+++ src/HRTimer.h
@@ -1,31 +1,37 @@
-#pragma once
+#ifndef MASTERMIND_HRTIMER_H
+#define MASTERMIND_HRTIMER_H
+
+#include <chrono>
 
 /// Contains helper classes.
 namespace Utilities
 {
-	/// High-resolution timer used to time program execution.
-	class HRTimer 
+	
+/// High-resolution timer used to time program execution.
+class HRTimer 
+{
+	typedef std::chrono::high_resolution_clock clock;
+	clock::time_point m_start;
+
+public:
+
+	HRTimer() { }
+
+	/// Starts timing.
+	void start()
 	{
-	private:
-		__int64 m_start;
-		__int64 m_stop;
-		double m_frequency;
-		double m_time;
+		m_start = clock::now();
+	}
 
-	public:
-		static double GetFrequency();
+	/// Stops timing and returns the number of seconds elapsed.
+	double stop()
+	{
+		std::chrono::duration<double> t1 = clock::now() - m_start;
+		return t1.count();
+	}
+};
 
-	public:
-		HRTimer();
-		//void Reset();
+} // namespace Utilities
 
-		/// Starts the timer.
-		void Start();
 
-		//void Pause();
-		//void Resume();
-
-		/// Stops the timer and returns the number of seconds elapsed.
-		double Stop();
-	};
-}
+#endif // MASTERMIND_HRTIMER_H
