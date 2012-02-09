0d1ead5bee1c05ce1a16356c42feab3ed8a4fb2d
fancitron <fancitron@gmail.com>
2012-02-09 03:22:21 +0000

    Migrated HeuristicCodeBreaker to HeuristicStrategy (without strategy tree building).

--- src/CodeBreaker.cpp
+++ src/CodeBreaker.cpp
@@ -36,104 +36,184 @@ Codeword CodeBreaker::MakeGuess()
 	return guess;
 }
 
-#if 0
-StrategyTreeMemoryManager *default_strat_mm = new StrategyTreeMemoryManager();
+//StrategyTreeMemoryManager *default_strat_mm = new StrategyTreeMemoryManager();
 
-CodeBreaker::CodeBreaker(Engine &engine)
-	: e(engine), m_rules(e.rules()), m_all(e.generateCodewords())
+// Copied from HeuristicCodeBreaker.cpp
+
+///////////////////////////////////////////////////////////////////////////
+// HeuristicCodeBreaker implementation
+//
+
+#if ENABLE_CALL_COUNTER
+static Utilities::CallCounter _call_counter("HeuristicCodeBreaker::MakeGuess", true);
+void PrintMakeGuessStatistics()
 {
-	assert(m_rules.valid());
-
-	// m_fp = fopen("E:/mastermind_stat.txt", "w");
-	m_fp = NULL;
-
-	Reset();
+	_call_counter.DebugPrint();
 }
-
-CodeBreaker::~CodeBreaker()
-{
-	if (m_fp) {
-		fclose((FILE*)m_fp);
-		m_fp = NULL;
-	}
-}
-
-void CodeBreaker::Reset()
-{
-	m_possibilities = m_all;
-	m_guessed = 0;
-	m_unguessed = ((unsigned short)1 << m_rules.colors()) - 1;
-	m_impossible = 0;
-	if (m_fp) {
-		fprintf((FILE*)m_fp, "R\n");
-	}
-}
-
-static int count_bits(unsigned short a)
-{
-	int n = 0;
-	for (; a; a >>= 1) {
-		if (a & 1)
-			n++;
-	}
-	return n;
-}
-
-void CodeBreaker::AddFeedback(const Codeword &guess, Feedback fb)
-{
-	m_possibilities = e.filterByFeedback(m_possibilities, guess, fb);
-	assert(m_possibilities.size() > 0);
-
-	unsigned short allmask = ((unsigned short)1 << m_rules.colors()) - 1;
-	m_unguessed &= ~e.getDigitMask(guess);
-	m_impossible = allmask & ~e.getDigitMask(m_possibilities.cbegin(), m_possibilities.cend());
-	m_guessed = allmask & ~m_unguessed & ~m_impossible;
-
-	if (m_fp) {
-		fprintf((FILE*)m_fp, "%x%x\n", count_bits(m_unguessed), count_bits(m_impossible));
-	}
-}
-
-CodewordList::const_iterator 
-CodeBreaker::makeObviousGuess(CodewordConstRange possibilities) const
-{
-	size_t count = possibilities.size();
-	if (count == 0)
-		return possibilities.end();
-
-	// If there are only two possibilities left, return the first one.
-	if (count <= 2) 
-		return possibilities.begin();
-
-	// If the number of possibilities is more than the number of distinct
-	// feedbacks, there will be no obvious guess.
-	size_t p = e.rules().pegs();
-	if (count > p*(p+3)/2)
-		return possibilities.end();
-
-	// Find the first obviously optimal guess (if any).
-#if 0
-	for (auto it = possibilities.begin(); it != possibilities.end(); ++it)
-	{
-#if 0
-		const Codeword &guess = *it;
-		FeedbackFrequencyTable freq;
-		FeedbackList fbl = e.compare(guess, possibilities);
-		e.countFrequencies(fbl.begin(), fbl.end(), freq);
-		if (freq.max() == 1) 
-			return it;
-#else
-		if (e.frequency(e.compare(*it, possibilities)).max() == 1)
-			return it;
 #endif
-	}
-	return possibilities.end(); // not found
-#else
-	return std::find_if(possibilities.begin(), possibilities.end(),
-		[&](const Codeword &guess) -> bool
-	{
-		return e.frequency(e.compare(guess, possibilities)).max() == 1;
-	});
+
+
+///////////////////////////////////////////////////////////////////////////
+// MakeGuess() implementation.
+
+#if 0
+MakeGuess(...)
+	state->NCandidates = candidates.size();
+	state->Guess = candidates[choose_i];
+#if ENABLE_CALL_COUNTER
+	_call_counter.AddCall(state->NCandidates*state->NPossibilities);
 #endif
+#endif
+
+#if 0
+template <class Heuristic>
+StrategyTreeNode* HeuristicCodeBreaker<Heuristic>::FillStrategy(
+	CodewordList::iterator first_possibility,
+	CodewordList::iterator last_possibility,
+	unsigned short unguessed_mask,
+	unsigned short impossible_mask,
+	const Codeword &first_guess,
+	int *progress)
+{
+	StrategyTreeState state;
+	if (first_guess.empty()) 
+	{
+		MakeGuess(first_possibility, last_possibility, 
+			unguessed_mask, impossible_mask, &state);
+	}
+	else 
+	{
+		state.NPossibilities = last_possibility - first_possibility;
+		state.NCandidates = -1;
+		state.Guess = first_guess;
+	}
+
+	StrategyTreeMemoryManager *mm = default_strat_mm;
+
+	Codeword guess = state.Guess;
+	FeedbackFrequencyTable freq;
+	e.partition(first_possibility, last_possibility, guess, freq);
+	//possibilities.Partition(guess, freq);
+	//FeedbackFrequencyTable freq(FeedbackList(guess, possibilities));
+
+	Feedback perfect = Feedback::perfectValue(e.rules());
+	StrategyTreeNode *node = StrategyTreeNode::Create(mm);
+	node->State = state;
+#if 0
+	int k = 0;
+	for (int fbv = 0; fbv <= freq.maxFeedback(); fbv++) 
+	{
+		Feedback fb(fbv);
+		int n = freq[fb];
+		if (n <= 0)
+			continue;
+
+		if (fb == perfect) 
+		{
+			(*progress)++;
+			double pcnt = (double)(*progress) / m_all.size();
+			printf("\rProgress: %3.0f%%", pcnt*100);
+			fflush(stdout);
+			node->AddChild(fb, StrategyTreeNode::Done());
+		} 
+		else 
+		{
+			Codeword t = Codeword::Empty();
+			// CodewordList filtered = possibilities.FilterByFeedback(guess, fb);
+			CodewordList::iterator first = first_possibility + k;
+			CodewordList::iterator last = first + n;
+			//CodewordList filtered(possibilities, k, n);
+			StrategyTreeNode *child = FillStrategy(
+				first,
+				last,
+				//filtered,
+				unguessed_mask & ~getDigitMask(guess),
+				((1<<m_rules.colors())-1) & ~getDigitMask(first, last),
+				t,
+				progress);
+			node->AddChild(fb, child);
+		}
+		k += n;
+	}
+#else
+	// Try write something like this:
+	auto p = partition(...);
+	for (auto it = p.begin(); it != p.end(); ++it)
+	{
+		CodewordPartition::Cell cell = *it;
+		if (cell.feedback() == perfect) 
+		{
+			(*progress)++;
+			double pcnt = (double)(*progress) / m_all.size();
+			printf("\rProgress: %3.0f%%", pcnt*100);
+			fflush(stdout);
+			node->AddChild(fb, StrategyTreeNode::Done());
+		}
+		else
+		{
+			StrategyTreeNode *child = FillStrategy(
+				cell.begin(),
+				cell.end(),
+				unguessed_mask & ~getDigitMask(guess),
+				((1<<m_rules.colors())-1) & ~getDigitMask(first, last),
+				Codeword::emptyValue(),
+				progress);
+			node->AddChild(fb, child);
+		}
+	}
+#endif
+
+	return node;
 }
+#endif
+
+#if 0
+template <class Heuristic>
+StrategyTree* HeuristicCodeBreaker<Heuristic>::BuildStrategyTree(const Codeword& first_guess)
+{
+	CodewordList all = m_all;
+	unsigned short impossible_mask = 0;
+	unsigned short unguessed_mask = (1 << m_rules.colors()) - 1;
+	int progress = 0;
+	StrategyTreeNode *node = FillStrategy(
+		all.begin(), all.end(), unguessed_mask, impossible_mask, first_guess, &progress);
+	return (StrategyTree*)node;
+
+	/*
+	partition the possibility list, i.e. reorder the elements so that
+		elements with same feedback are put together;
+	foreach (feedback in all feedbacks) {
+		filter possibilities with the feedback (locate the partition);
+		find the first remaining element in possibilities;
+		use this item as the guess, and push it into strategy tree;
+		the remaining possibilities are the partition;
+		call BuildStrategyTree() recursively;
+	}
+
+	return (the strategy tree built this way);
+	*/
+}
+#endif
+
+#if 0
+// Minimize steps heuristic
+int Heuristics::MinimizeSteps::compute(const FeedbackFrequencyTable &freq)
+{
+	int minsteps = 0;
+	for (int fbv = 0; fbv <= freq.maxFeedback(); fbv++) {
+		Feedback fb(fbv);
+		int n = freq[fb];
+		if (n > 0) {
+			if (fb == Feedback::Perfect(4)) {
+				//
+			} else {
+				minsteps += n;
+				minsteps += partition_score[n];
+			}
+		}
+	}
+	return minsteps;
+}
+
+int Heuristics::MinimizeSteps::partition_score[10000];
 #endif
