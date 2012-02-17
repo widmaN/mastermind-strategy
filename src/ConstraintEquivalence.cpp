43d5c42846d85529266ada8f355e873fadf4152a
fancitron <fancitron@gmail.com>
2012-02-17 10:04:26 +0000

    Improved ConstraintEquivalenceFilter implementation.

--- src/ConstraintEquivalence.cpp
+++ src/ConstraintEquivalence.cpp
@@ -6,37 +6,123 @@
 
 #include "Engine.hpp"
 #include "Permutation.hpp"
-#include "util/intrinsic.hpp"
 #include "Equivalence.hpp"
+
+#include "util/intrinsic.hpp"
 #include "util/call_counter.hpp"
+#include "util/aligned_allocator.hpp"
 
 REGISTER_CALL_COUNTER(ConstraintEquivalence)
 // REGISTER_CALL_COUNTER(ConstraintEquivalenceIdentity)
 REGISTER_CALL_COUNTER(ConstraintEquivalencePermutation)
 REGISTER_CALL_COUNTER(ConstraintEquivalenceCrossout)
 
-using namespace Mastermind;
+namespace Mastermind {
+
+struct MyColorMask
+{
+	unsigned short value;
+
+	MyColorMask() : value(0) { }
+
+	MyColorMask(int count) : value((1 << count) - 1) 
+	{ 
+		assert(count >= 0 && count <= MM_MAX_COLORS);
+	}
+	
+	bool operator [] (int color) const
+	{
+		assert(color >= 0 && color <= MM_MAX_COLORS);
+		return (value & (1 << color)) != 0;
+	}
+
+	int smallest() const 
+	{
+		return value == 0 ? -1 : util::intrinsic::bit_scan_forward(value);
+	}
+
+	void reset(int color)
+	{
+		assert(color >= 0 && color <= MM_MAX_COLORS);
+		value &= ~(1 << color);
+	}
+
+	void reset() 
+	{
+		value = 0;
+	}
+
+	bool unique() const
+	{
+		return value && (value & (value - 1)) == 0;
+	}
+
+	bool empty() const { return value == 0; }
+
+#if 0
+	int count() const
+	{
+		int n = 0;
+		for (int i = 0; i < MM_MAX_COLORS; ++i)
+		{
+			if (value & (1 << i))
+				++n;
+		}
+		return n;
+	}
+
+	void set_count(int count)
+	{
+		assert(count >= 0 && count <= MM_MAX_COLORS);
+		value = (1 << count) - 1;
+	}
+#endif
+};
+
+//typedef MyColorMask ColorMask;
+
+/// Represents an incremental constraint equivalence filter.
+class ConstraintEquivalenceFilter : public EquivalenceFilter
+{
+	Engine &e;
+	MyColorMask free_colors;
+
+	std::vector<
+		CodewordPermutation,
+		util::aligned_allocator<CodewordPermutation,16>
+	> pp;
+
+public:
+
+	ConstraintEquivalenceFilter(Engine &engine);
+
+	virtual EquivalenceFilter* clone() const
+	{
+		return new ConstraintEquivalenceFilter(*this);
+	}
+
+	virtual CodewordList get_canonical_guesses(
+		CodewordConstRange candidates) const;
+
+	virtual void add_constraint(
+		const Codeword &guess,
+		Feedback response, 
+		CodewordConstRange remaining
+		);
+};
 
 /// Initializes a constraint equivalence filter.
 ConstraintEquivalenceFilter::ConstraintEquivalenceFilter(Engine &engine)
-	: e(engine), rules(e.rules())
+	: e(engine), free_colors(e.rules().colors())
 {
 	// Generate all peg permutations, and associate with each peg 
-	// permutation a fully-free partial color permutation.
-
-	// Note: the peg permutations stored are in fact the inverse of 
-	// the traditional permutations. But since we generate all such
-	// permutations, we don't need to explicitly compute the inverse.
-
-	CodewordPermutation p(rules);
-	for (int i = 0; i < rules.pegs(); ++i)
-		p.peg[i] = (char)i;
-
+	// permutation a fully unrestricted partial color permutation.
+	CodewordPermutation p;
 	do
 	{
 		pp.push_back(p);
 	} 
-	while (std::next_permutation(p.peg + 0, p.peg + rules.pegs()));
+	while (std::next_permutation(p.peg + 0, p.peg + e.rules().pegs()));
 }
 
 // Returns a list of canonical guesses given the current constraints.
@@ -45,25 +131,16 @@ CodewordList ConstraintEquivalenceFilter::get_canonical_guesses(
 {
 	bool verbose = false;
 
-	// Let u be the number of colors fixed so far.
-	int u = 0;
-	for (u = 0; u < rules.colors(); ++u)
-	{
-		if (pp[0].color[u] < 0)
-			break;
-	}
-
 #if 1
 	// Optimization: if there is only one peg permutation left (in
-	// which case it must be the identity permutation) and the color
-	// permutation is "almost completely" specified, then the color
-	// permutation must be identity too, and there is no codeword
-	// to filter out.
-	if (pp.size() == 1 && u >= rules.colors() - 1)
+	// which case it must be the identity permutation) and there are
+	// no free colors left, then the color permutation must be 
+	// identity too, and there is no codeword to filter out.
+	if (pp.size() == 1 && free_colors.empty())
 		return CodewordList(candidates.begin(), candidates.end());
 #endif
 
-	// Check each non-crossed codeword in the list.
+	// Check each candidate in turn.
 	size_t n = candidates.size();
 	CodewordList canonical;
 	canonical.reserve(n);
@@ -88,57 +165,44 @@ CodewordList ConstraintEquivalenceFilter::get_canonical_guesses(
 				debug_stop = true;
 #endif
 
-			// Get a bit-mask of the unmapped colors in the permutation.
-			CodewordPermutation p = pp[j];
 
 			// Permute the pegs and colors of the candidate.
-			// Unmapped colors are kept unchanged.
+			// Free colors are kept unchanged.
 			// Optimization: if this is the identity permutation,
 			// then needn't permute.
+			CodewordPermutation p = pp[j];
 			Codeword permuted_candidate = 
-				(j == 0)? candidate : p.permute(candidate);
+				(j == 0)? candidate : p.permute_pegs(candidate);
 
 			// Check the color on each peg in turn.
 			// Take, for example, 1223. It must be able to map to 1123 and
 			// show that it's not canonical.
-			int free_color = u;
-			for (int k = 0; k < rules.pegs(); ++k)
+			MyColorMask free_from = free_colors, free_to = free_colors;
+			//int free_color = u;
+			for (int k = 0; k < e.rules().pegs(); ++k)
 			{
-				// Let c be the color on peg k of the permuted candidate.
+				// Let c be the color on peg k of the peg-permuted candidate.
 				int c = permuted_candidate[k];
 
-				// If c is not mapped, find the smallest, unmapped color
-				// c', and swap c with c' in the permuted candidate.
-				if (p.color[c] < 0)
+				// If c is free, map it to the smallest available free color,
+				// and update the permutation. Otherwise, map it according 
+				// to the current permutation.
+				if (free_from[c])
 				{
-					// Find the smallest unmapped color.
-#if 0
-					int cc = -1;
-					for (int l = 0; l < rules.colors(); ++l)
-					{
-						if (p.color[l] < 0) 
-						{
-							cc = l;
-							break;
-						}
-					}
-#else
-					int cc = free_color++;
-#endif
+					// Find the smallest available free color.
+					int cc = free_to.smallest();
 
-					for (int l = k; l < rules.pegs(); ++l)
-					{
-						if (permuted_candidate[l] == c)
-							permuted_candidate.set(l, cc);
-						else if (permuted_candidate[l] == cc)
-							permuted_candidate.set(l, c);
-					}
-					p.color[cc] = cc;
-					c = cc;
+					// Map c to cc.
+					p.color[c] = cc;
+
+					// Clear the respective free-color indicator.
+					free_from.reset(c);
+					free_to.reset(cc);
 				}
+				c = p.color[c];
 
-				// The color, c, must be lexicographically greater than
-				// or equal to the corresponding color in the candidate
+				// The mapped color, c, must be lexicographically greater
+				// than or equal to the corresponding color in the candidate
 				// for the candidate to be canonical.
 				if (c < candidate[k])
 				{
@@ -188,17 +252,29 @@ void ConstraintEquivalenceFilter::add_constraint(
 		CodewordPermutation &p = pp[i];
 
 		// Permute the pegs in the guess.
-		Codeword c = p.permute_pegs(guess);
+		Codeword permuted = p.permute_pegs(guess);
 
 		// Try to map the color on each peg onto itself.
 		// @bug The following code is not correct in some cases;
 		// see the modified version for the correct one.
+		MyColorMask free_from = free_colors, free_to = free_colors;
 		bool ok = true;
-		for (int j = 0; j < rules.pegs() && ok; ++j)
+		for (int j = 0; j < e.rules().pegs() && ok; ++j)
 		{
-			if (p.color[c[j]] == -1)
-				p.color[c[j]] = guess[j];
-			else if (p.color[c[j]] != guess[j])
+			if (free_from[permuted[j]])
+			{
+				if (!free_to[guess[j]])
+				{
+					ok = false;
+				}
+				else
+				{
+					p.color[permuted[j]] = guess[j];
+					free_from.reset(permuted[j]);
+					free_to.reset(guess[j]);
+				}
+			}
+			else if (p.color[permuted[j]] != guess[j])
 				ok = false;
 		}
 
@@ -220,6 +296,24 @@ void ConstraintEquivalenceFilter::add_constraint(
 		}
 	}
 
+	// Restrict the color mask.
+	for (int i = 0; i < e.rules().pegs(); ++i)
+	{
+		free_colors.reset(guess[i]);
+	}
+
+	// If all but one colors are restricted, the last color is
+	// automatically restricted because it can only map to itself.
+	if (free_colors.unique())
+		free_colors.reset();
+
 	// After a few constraints, only the identity permutation
 	// will remain.
 }
+
+EquivalenceFilter* CreateConstraintEquivalenceFilter(Engine &e)
+{
+	return new ConstraintEquivalenceFilter(e);
+}
+
+} // namespace Mastermind
--- src/Equivalence.hpp
+++ src/Equivalence.hpp
@@ -3,10 +3,8 @@
 
 // This file defines an interface for equivalence filters.
 
-// #include <memory>
 #include "Engine.hpp"
 #include "Permutation.hpp"
-#include "util/aligned_allocator.hpp"
 
 namespace Mastermind {
 
@@ -56,35 +54,7 @@ public:
 };
 #endif
 
-/// Represents an incremental constraint equivalence filter.
-class ConstraintEquivalenceFilter : public EquivalenceFilter
-{
-	Engine &e;
-	Rules rules;
-	std::vector<
-		CodewordPermutation,
-		util::aligned_allocator<CodewordPermutation,16>
-	> pp;
-
-public:
-	ConstraintEquivalenceFilter(Engine &engine);
-
-	virtual EquivalenceFilter* clone() const
-	{
-		//return std::unique_ptr<EquivalenceFilter>(
-		//	new ConstraintEquivalenceFilter(*this));
-		return new ConstraintEquivalenceFilter(*this);
-	}
-
-	virtual CodewordList get_canonical_guesses(
-		CodewordConstRange candidates) const;
-
-	virtual void add_constraint(
-		const Codeword &guess,
-		Feedback response, 
-		CodewordConstRange remaining
-		);
-};
+EquivalenceFilter* CreateConstraintEquivalenceFilter(Engine &e);
 
 } // namespace Mastermind
 
