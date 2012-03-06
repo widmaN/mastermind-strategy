8272b3e1b44e96201b572aec1f556b5b77b27714
fancitron <fancitron@gmail.com>
2012-03-06 03:22:12 +0000

    Tested and rejected TinyFrequencyUpdater implementation.

--- src/Compare.cpp
+++ src/Compare.cpp
@@ -1,6 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Codeword comparison routines.
 
+#include <cassert>
+#include <utility>
 #include "util/simd.hpp"
 #include "util/intrinsic.hpp"
 #include "util/call_counter.hpp"
@@ -10,73 +12,34 @@ REGISTER_CALL_COUNTER(Comparison)
 
 namespace Mastermind {
 
-///////////////////////////////////////////////////////////////////////////
-// Generic comparison routine for repeatable codewords.
-
-// Precompute a conversion table that converts (nA<<4|nAB) -> feedback.
-// Both nA and nAB must be >= 0 and <= 15.
-struct generic_feedback_mapping_t
-{
-	Feedback table[0x100];
-
-	generic_feedback_mapping_t()
-	{
-		for (int i = 0; i < 0x100; i++)
-		{
-			int nA = i >> 4;
-			int nAB = i & 0xF;
-			table[i] = Feedback(nA, nAB - nA);
-		}
-	}
-};
-
-static const generic_feedback_mapping_t generic_feedback_mapping;
-
-
-///////////////////////////////////////////////////////////////////////////
-// Specialized comparison routine for NON-repeatable codewords.
-
-// Pre-computed table that converts a comparison bitmask of
-// non-repeatable codewords into a feedback.
-//
-// Note that the performance impact of the table lookup can be subtle.
-// The VC++ compiler tends to generate a MOVZX (32-bit) or MOVSXD (64-bit)
-// instruction if the integer sizes do not match. What's worse, because
-// an SSE2 intrinsic function in use returns an <code>int</code>, there
-// is no way to get rid of the MOVSXD instruction which deteriates
-// performance by 30%!
-//
-// Hence, the best we could do is probably to compile under 32-bit, and 
-// access the table directly.
-struct norepeat_feedback_mapping_t
-{
-	Feedback table[0x10000];
-
-	norepeat_feedback_mapping_t()
-	{
-		for (int i = 0; i < 0x10000; i++)
-		{
-			int nA = util::intrinsic::pop_count((unsigned short)(i >> MM_MAX_COLORS));
-			int nAB = util::intrinsic::pop_count((unsigned short)(i & ((1<<MM_MAX_COLORS)-1)));
-#if 0
-			table[i] = nAB*(nAB+1)/2+nA;
-#else
-			table[i] = Feedback(nA, nAB - nA);
-#endif
-		}
-	}
-};
-
-static const norepeat_feedback_mapping_t norepeat_feedback_mapping;
-
-
+/// Codeword comparer for generic codewords (with or without repetition).
 class GenericComparer
 {
+	// Lookup table that converts (nA<<4|nAB) -> feedback.
+	// Both nA and nAB must be >= 0 and <= 15.
+	struct lookup_table_t
+	{
+		Feedback table[0x100];
+
+		lookup_table_t()
+		{
+			for (int i = 0; i < 0x100; i++)
+			{
+				int nA = i >> 4;
+				int nAB = i & 0xF;
+				table[i] = Feedback(nA, nAB - nA);
+			}
+		}
+	};
+
+	static const lookup_table_t lookup;
+
 	typedef util::simd::simd_t<uint8_t,16> simd_t;
 
 	simd_t secret;
 
 public:
+
 	GenericComparer(const Codeword &_secret) : secret(_secret.value())
 	{
 		// Change 0xff in secret to 0x0f.
@@ -144,13 +107,45 @@ public:
 		int nAB = sum(min(guess, secret) & mask_colors);
 #endif
 
-		Feedback nAnB = generic_feedback_mapping.table[(nA<<4)|nAB];
+		Feedback nAnB = lookup.table[(nA<<4)|nAB];
 		return nAnB;
 	}
 };
 
+const GenericComparer::lookup_table_t GenericComparer::lookup;
+
+/// Specialized codeword comparer for codewords without repetition.
 class NoRepeatComparer
 {
+	// Pre-computed table that converts a comparison bitmask of
+	// non-repeatable codewords into a feedback.
+	//
+	// Note that the performance impact of the table lookup can be subtle.
+	// The VC++ compiler tends to generate a MOVZX (32-bit) or MOVSXD (64-bit)
+	// instruction if the integer sizes do not match. What's worse, because
+	// an SSE2 intrinsic function in use returns an <code>int</code>, there
+	// is no way to get rid of the MOVSXD instruction which deteriates
+	// performance by 30%!
+	//
+	// Hence, the best we could do is probably to compile under 32-bit, and 
+	// access the table directly.
+	struct lookup_table_t
+	{
+		Feedback table[0x10000];
+
+		lookup_table_t()
+		{
+			for (int i = 0; i < 0x10000; i++)
+			{
+				int nA = util::intrinsic::pop_count((unsigned short)(i >> MM_MAX_COLORS));
+				int nAB = util::intrinsic::pop_count((unsigned short)(i & ((1<<MM_MAX_COLORS)-1)));
+				table[i] = Feedback(nA, nAB - nA);
+			}
+		}
+	};
+
+	static const lookup_table_t lookup;
+
 	typedef util::simd::simd_t<char,16> simd_t;
 	
 	simd_t secret;
@@ -187,11 +182,14 @@ public:
 	 */
 	Feedback operator () (const Codeword &guess) const 
 	{
-		return norepeat_feedback_mapping.table
+		return lookup.table
 			[ util::simd::byte_mask(simd_t(guess.value()) == secret) ];
 	}
 };
 
+const NoRepeatComparer::lookup_table_t NoRepeatComparer::lookup;
+
+/// Function object that appends a feedback to a feedback list.
 class FeedbackUpdater
 {
 	Feedback * feedbacks;
@@ -206,6 +204,7 @@ public:
 	}
 };
 
+/// Function object that increments the frequency statistic of a feedback.
 class FrequencyUpdater
 {
 	unsigned int * freq;
@@ -223,16 +222,81 @@ public:
 	}
 };
 
+/// Function object that invokes two functions.
+template <class T1, class T2>
+struct CompositeUpdater
+{
+	T1 u1;
+	T2 u2;
+
+	CompositeUpdater(T1 updater1, T2 updater2) 
+		: u1(updater1), u2(updater2) { }
+
+	void operator () (const Feedback &fb)
+	{
+		u1(fb);
+		u2(fb);
+	}
+};
+
+#if 0
+/// Specialized function object that keeps track of frequency statistics 
+/// for no more than 16 distinct feedbacks and no more than 15 frequencies.
+class TinyFrequencyUpdater
+{
+	unsigned int * _freq;
+	size_t _size;
+	unsigned long long _counter;
+
+	TinyFrequencyUpdater& operator = (const TinyFrequencyUpdater &) { }
+
+public:
+	
+	TinyFrequencyUpdater(unsigned int *freq, size_t size) 
+		: _freq(freq), _size(size), _counter(0)
+	{
+		assert(size <= 16);
+	}
+
+	TinyFrequencyUpdater(TinyFrequencyUpdater &u)
+		: _freq(u._freq), _size(u._size), _counter(u._counter)
+	{
+		u._freq = 0;
+	}
+
+	~TinyFrequencyUpdater()
+	{
+		if (_freq)
+		{
+			for (size_t i = 0; i < _size; ++i)
+			{
+				_freq[i] = (unsigned int)(_counter >> (i * 4)) & 0x0FU;
+			}
+		}
+	}
+
+	void operator () (const Feedback &fb) 
+	{
+		assert(fb.value() < 16);
+		_counter += (1ULL << (fb.value() * 4));
+	}
+};
+#endif
+
+/// Compares a secret to a list of codewords using @c Comparer, and 
+/// processes each feedback using @c Updater.
 template <class Comparer, class Updater>
-void compare_codewords(
+static void compare_codewords(
 	const Codeword &secret,
 	const Codeword *first,
 	const Codeword *last,
 	Updater _update)
 {
+	// The following redundant copy is to make VC++ happy treat the value
+	// of @c _update as constant in the loop.
+	Updater update(_update);
+
 	Comparer compare(secret);
-	Updater update(_update); // this redundant copy is to make VC++ happy that
-	                         // the value of update will not change in the loop
 	size_t count = last - first;
 	const Codeword *guesses = first;
 	for (; count > 0; --count)
@@ -242,70 +306,35 @@ void compare_codewords(
 	}
 }
 
-// SSE2-based implementation for comparing generic codewords.
-// Repeated colors are allowed.
-#if 0
-static void compare_long_codeword_generic(
-	const Rules & /* rules */,
-	const Codeword &_secret,
-	const Codeword *_first,
-	const Codeword *_last,
-	Feedback result[])
+template <class Comparer>
+static void compare_codewords_impl(
+	const Codeword &secret,
+	const Codeword *first,
+	const Codeword *last,
+	Feedback *result,
+	unsigned int *freq,
+	size_t size)
 {
-	UPDATE_CALL_COUNTER(Comparison, (unsigned int)(_last - _first));
-#if 0
-	if (_last - _first == 1)
+	if (result && freq)
 	{
-		int kk = 1;
+		CompositeUpdater<FeedbackUpdater,FrequencyUpdater> 
+			update(FeedbackUpdater(result), FrequencyUpdater(freq, size));
+		compare_codewords<Comparer>(secret, first, last, update);
 	}
-#endif
-
-	using namespace util::simd;
-	typedef util::simd::simd_t<uint8_t,16> simd_t;
-
-	simd_t secret = _secret.value();
-
-	// Change 0xff in secret to 0x0f.
-	secret &= (uint8_t)0x0f;
-
-	// Create mask for use later.
-	const simd_t mask_pegs = fill_left<MM_MAX_PEGS>((uint8_t)0x01);
-	const simd_t mask_colors = fill_right<MM_MAX_COLORS>((uint8_t)0xff);
-
-	// Note: we write an explicit loop since std::transform() is too
-	// slow, because VC++ does not inline the lambda expression, thus
-	// making each iteration a CALL with arguments passed on the stack.
-	const simd_t *first = reinterpret_cast<const simd_t *>(_first);
-	const simd_t *last = reinterpret_cast<const simd_t *>(_last);
-	for (const simd_t *guesses = first; guesses != last; ++guesses)
+	else if (freq)
 	{
-		const simd_t &guess = *guesses;
-
-		// Compute nA.
-		// It turns out that there's a significant (20%) performance
-		// hit if <code>nA = sum_high(...)</code> is changed to
-		// <code>nA = sum(...)</code>. In the latter case, VC++
-		// VC++ generates an extra (redundant) memory read.
-		// The reason is unclear. (Obviously there are still free
-		// XMM registers.)
-#if MM_MAX_PEGS <= 8
-		int nA = sum_high((secret == guess) & mask_pegs);
-#else
-		int nA = sum((secret == guess) & mask_pegs);
-#endif
-
-		// Compute nAB.
-#if MM_MAX_COLORS <= 8
-		int nAB = sum_low(min(secret, guess) & mask_colors);
-#else
-		int nAB = sum(min(secret, guess) & mask_colors);
-#endif
-
-		Feedback nAnB = generic_feedback_mapping.table[(nA<<4)|nAB];
-		*(result++) = nAnB;
+		FrequencyUpdater update(freq, size);
+		compare_codewords<Comparer>(secret, first, last, update);
+	}
+	else if (result)
+	{
+		FeedbackUpdater update(result);
+		compare_codewords<Comparer>(secret, first, last, update);
 	}
 }
-#else
+
+// SSE2-based implementation for comparing generic codewords.
+// Repeated colors are allowed.
 static void compare_long_codeword_generic(
 	const Rules & /* rules */,
 	const Codeword &secret,
@@ -313,10 +342,13 @@ static void compare_long_codeword_generic(
 	const Codeword *last,
 	Feedback result[])
 {
+#if 0
 	FeedbackUpdater update(result);
 	compare_codewords<GenericComparer>(secret, first, last, update);
-}
+#else
+	compare_codewords_impl<GenericComparer>(secret, first, last, result, 0, 0);
 #endif
+}
 
 // Comparison routine for codewords without repetition.
 static void compare_long_codeword_norepeat(
@@ -326,8 +358,12 @@ static void compare_long_codeword_norepeat(
 	const Codeword *last,
 	Feedback result[])
 {
+#if 0
 	FeedbackUpdater update(result);
 	compare_codewords<NoRepeatComparer>(secret, first, last, update);
+#else
+	compare_codewords_impl<NoRepeatComparer>(secret, first, last, result, 0, 0);
+#endif
 }
 
 void compare_frequencies_generic(
@@ -338,8 +374,12 @@ void compare_frequencies_generic(
 	unsigned int freq[],
 	size_t size)
 {
+#if 0
 	FrequencyUpdater update(freq, size);
 	compare_codewords<GenericComparer>(secret, first, last, update);
+#else
+	compare_codewords_impl<GenericComparer>(secret, first, last, 0, freq, size);
+#endif
 }
 
 void compare_frequencies_norepeat(
@@ -350,8 +390,23 @@ void compare_frequencies_norepeat(
 	unsigned int freq[],
 	size_t size)
 {
-	FrequencyUpdater update(freq, size);
-	compare_codewords<NoRepeatComparer>(secret, first, last, update);
+#if 0
+	size_t count = last - first;
+	if (count <= 15 && size <= 16)
+	{
+		TinyFrequencyUpdater update(freq, size);
+		compare_codewords<NoRepeatComparer>(secret, first, last, update);
+	}
+	else
+#endif
+	{
+#if 0
+		FrequencyUpdater update(freq, size);
+		compare_codewords<NoRepeatComparer>(secret, first, last, update);
+#else
+		compare_codewords_impl<NoRepeatComparer>(secret, first, last, 0, freq, size);
+#endif
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////
